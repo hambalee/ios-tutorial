d3f902a8b6c05b565dee928e4f94dd7beca76e04
hambalee <hambalee@outlook.com>
2022-10-18 10:50:35 +0700

    insert image and text field

--- iOS Tutorial/ViewController.swift
+++ iOS Tutorial/ViewController.swift
@@ -12,7 +12,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.blue
     }
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        print("viewWillAppear")
+//    }
+//    override func viewDidAppear(_ animated: Bool) {
+//        print("viewDidAppear")
+//    }
+//    override func viewWillDisappar(_ animated: Bool) {
+//        print("viewWillDisappar")
+//    }
+//    override func viewDidDisappar(_ animated: Bool) {
+//        print("viewDidDisappar")
+//    }
 
 
 }
